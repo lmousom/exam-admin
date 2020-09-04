132007849f7bf0d7419af98b65e8e1553f1168fa
Latiful Mousom <latifulmousom@gmail.com>
2020-09-04 22:06:13 +0530

    Updates

---
+++ precache-manifest.75ca454fffb84728e2be4bf8281c132b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ae06cf00c7294e0e8ab64bc5bee533",
+    "url": "/exam-admin/index.html"
+  },
+  {
+    "revision": "095ae06e7395ba915641",
+    "url": "/exam-admin/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "839541cde78ad3abd20f",
+    "url": "/exam-admin/static/css/main.e2602862.chunk.css"
+  },
+  {
+    "revision": "095ae06e7395ba915641",
+    "url": "/exam-admin/static/js/2.c878e2ce.chunk.js"
+  },
+  {
+    "revision": "d645659a71ebf4d66d9b63fd5624be43",
+    "url": "/exam-admin/static/js/2.c878e2ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "839541cde78ad3abd20f",
+    "url": "/exam-admin/static/js/main.ed990990.chunk.js"
+  },
+  {
+    "revision": "83fa0fef612d15b63199",
+    "url": "/exam-admin/static/js/runtime-main.d756ba35.js"
+  }
+]);
